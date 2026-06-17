b74f25be141ddb211340ee334b9cba613ff298af
413x-4rtur0 <413x-4rtur0@users.noreply.github.com>
2026-06-17 18:34:27 -0500

    Improve collector configuration and database resilience

--- pc_index_collector.py
+++ pc_index_collector.py
@@ -2,7 +2,7 @@
 from playwright.sync_api import sync_playwright
 from pc_common import *
 
-MAX_PAGES_PER_GROUP = int(os.environ.get("PC_MAX_PAGES_PER_GROUP", "20"))
+MAX_PAGES_PER_GROUP = env_int("PC_MAX_PAGES_PER_GROUP", "20", minimum=1)
 
 GROUPS = [
     {"name": "Programadas", "radio_id": "btnradio2"},
